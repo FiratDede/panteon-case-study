bcb6734a1f0b425ca4b2841025f380668464fc3a
FIRAT DEDE <dede.firat.dede@gmail.com>
2026-05-18 23:46:54 +0300

    chore(server): add local seed script configuration

--- apps/server/src/config/env.ts
+++ apps/server/src/config/env.ts
@@ -5,10 +5,10 @@ const envSchema = z.object({
   NODE_ENV: z.enum(["development", "test", "production"]).default("development"),
   PORT: z.coerce.number().int().positive().default(4000),
   CLIENT_ORIGIN: z.string().url().default("http://localhost:5173"),
-  DATABASE_URL: z.string().min(1),
+  DATABASE_URL: z.string().url().default("postgresql://postgres:123456@localhost:5432/panteon_task_postgresql_db?schema=public"),
   REDIS_URL: z.string().url().default("redis://localhost:6379"),
   MONGODB_URL: z.string().url().default("mongodb://localhost:27017"),
-  MONGODB_DB: z.string().min(1).default("panteon_leaderboard")
+  MONGODB_DB: z.string().min(1).default("panteon_task_mongo_db")
 });
 
 export const env = envSchema.parse(process.env);
---
+++ apps/server/src/testScripts/seedPlayers.ts
@@ -0,0 +1,31 @@
+import { randomInt } from "crypto";
+import pLimit from "p-limit";
+import { chunkArray } from "../common/utils/chunk";
+import { prisma } from "../db/prisma"
+if(process.argv.length < 3) {
+    throw new Error("You must specify how many players you want to create")
+}
+const totalCountOfPlayers = Number(process.argv[2])
+
+
+  const limit = pLimit(5); // aynı anda max 5 işlem
+
+  const data = Array.from({ length: totalCountOfPlayers }, (_, i) => {
+    return {
+      playerName: `player-${i + 1}`,
+    }
+  });
+
+  const chunks = chunkArray(data, 1000);
+
+  await Promise.all(
+    chunks.map(chunk =>
+      limit(() =>
+        prisma.player.createMany({
+          data: chunk,
+          skipDuplicates: true,
+        })
+      )
+    )
+  );
+  console.log("Players added.")
